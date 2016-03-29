a0c9c6688a6339e073f448e4bc78f1dbe2f187e1
Barnabas Bulpett <bbulpett@charter.net>
2016-03-28 23:16:03 -0400

    Create Room model, modify authentication defaults

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   get '/hello/sign_out' => 'hello/authentication/sessions#sign_out'
   mount Hello::Engine => "/hello"
-  root to: 'root#index'
+  # root to: 'root#index'
   root to: 'rooms#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
